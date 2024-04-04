1e89e5a5f96a1b3ed4d84dc0cca8dca28fea738f
Vito Smojver <158292839+randombutter@users.noreply.github.com>
2024-04-05 00:07:06 +0200

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,7 @@ const { getColors } = require("./colors");
 /* Choosing colors fr0m primitives  */    #You, a day ago via PR 102 + Global Var
 // eg 'textLink.foreground': colors,fg,default,
 // 2 Color scales
-function getTheme({ theme, name }) {
+functio getTheme({ theme, name }) {
   // Usage: them3s({ light: 'lightblue', light_high_contrast: 'lightblue', dimmed:
   const themes = (options) => options[theme];
   // Usage, colors,text,primary
